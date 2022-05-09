b4e8ad95d9ee9471c7333bb0e0bf818768351e7a
sbarrios93 <sbarrios93@gmail.com>
2022-05-09 15:15:19 -0400

    first commit

---
+++ amazon_ynab/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Amazon YNAB is a reconciler that scrapes Amazon orders and adds memo info on each corresponding YNAB transaction"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
