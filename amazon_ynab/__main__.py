de2a3351c0f3a6fbc62e89010519251cdf0fc5f1
sbarrios93 <sbarrios93@gmail.com>
2022-05-13 20:47:44 -0400

    feat: :construction: init ynab client

--- amazon_ynab/__main__.py
+++ amazon_ynab/__main__.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import typer
 from rich.console import Console
 
@@ -6,6 +8,7 @@ from amazon_ynab.amazon.amazon_client import AmazonClient
 from amazon_ynab.paths.common_paths import get_paths
 from amazon_ynab.paths.utils import check_if_path_exists
 from amazon_ynab.utils import utils
+from amazon_ynab.ynab.ynab_client import YNABClient
 
 PATHS: dict[str, str] = get_paths()
 
@@ -91,18 +94,61 @@ def run(  # noqa
 
     secrets: dict[str, dict[str, str]] = utils.load_secrets(path_to_secrets)
 
+    # parse days back to a datetime date
+    cutoff_date: datetime = utils.days_back_to_cutoff_date(days_back)
+
     # init client
     amazon_client = AmazonClient(
-        secrets["amazon"]["username"],
-        secrets["amazon"]["password"],
+        user_credentials=(secrets["amazon"]["username"], secrets["amazon"]["password"]),
         run_headless=headless,
-        days_back=days_back,
-        today_inclusive=today_inclusive,
+        cutoff_date=cutoff_date,
         short_items=short_items,
         words_per_item=words_per_item,
     )
 
-    amazon_client.run_pipeline()
+    # amazon_client.run_pipeline()
+
+    ynab_client = YNABClient(secrets["ynab"]["token"], cutoff_date)
+
+    # prepare the YNAB Client to be used. First call the YNAB Client read the budgets.
+    ynab_client.prepare_client()
+
+    # if there is an entry on the secrets file that has a budget id
+    # we can use that one as long as it is contained in the budgets
+    if secrets["ynab"]["budget_id"]:
+        budget_matched = False
+        for _, budget_id in ynab_client.all_budgets.items():
+            if budget_id == secrets["ynab"]["budget_id"]:
+                ynab_client.selected_budget = budget_id
+                budget_matched = True
+                console.print(
+                    f"[green]✔[/] Budget ID matched: {ynab_client.selected_budget}"
+                )
+                break
+        if not budget_matched:
+            console.print(
+                "[red]✘[/] Budget ID found on secrets file, but it is not in the YNAB budgets list, if you want to use a specific budget id, please add it to the secrets file. You can also set it to null on the secrets file and the program will prompt you for a budget id."
+            )
+            raise typer.Exit()
+    elif len(ynab_client.all_budgets) == 1:
+        # if we only have one budget and no budget id, we can use that one
+
+        console.print(
+            f"[yellow]WARNING:[/] No budget ID found on secrets file, using the only budget ID found: {list(ynab_client.all_budgets.keys())[0]}"
+        )
+        ynab_client.selected_budget = ynab_client.all_budgets[
+            list(ynab_client.all_budgets.keys())[0]
+        ]
+    else:
+        # if no budget id is found in the secrets file, and there is more than one budget prompt the user to select one
+        console.print(
+            "[yellow]No budget ID found in secrets file, please select one[/]"
+        )
+        ynab_client.prompt_user_for_budget_id()
+
+        ynab_client.selected_budget = secrets["ynab"]["budget_id"]
+    # for budget_name, budget_id in ynab_client.all_budgets.items():
+
     print("timestop")
 
 
---
+++ amazon_ynab/ynab/ynab_client.py
@@ -0,0 +1,116 @@
+from typing import Any
+
+import re
+from datetime import datetime
+
+import requests
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.prompt import Prompt
+from rich.rule import Rule
+
+
+class YNABClient:
+    def __init__(self, token: str, since_date: datetime) -> None:
+        self.token = token
+        self.since_date = since_date
+
+        self.urls: dict[str, str] = {}
+
+        self.urls["base"] = "https://api.youneedabudget.com/v1"
+        self.urls["budgets"] = self.urls["base"] + "/budgets"
+        self.urls["transactions"] = self.urls["budgets"] + "/{}/transactions"
+
+        self.request_headers: dict[str, str] = {
+            "Authorization": f"Bearer {self.token}",
+        }
+
+        self.all_budgets: dict[str, str] = {}
+        self.selected_budget: str | None = None  # budget id in the API
+
+        self.transactions_to_match: dict[str, Any] = {}
+
+    def _get_budgets(self) -> Any:
+        """
+        Gets all budgets associated with the user's account.
+        """
+        url = self.urls["budgets"]
+        response = requests.get(url, headers=self.request_headers)
+        return response.json()["data"]["budgets"]
+
+    def _parse_budgets(self) -> None:
+        """
+        Selects the budget that contains the transactions.
+        """
+
+        for budget in self._get_budgets():
+            self.all_budgets[budget["name"]] = budget["id"]
+
+    def prepare_client(self) -> None:
+        """
+        Prepares the client to be used.
+        """
+        self._parse_budgets()
+
+    def prompt_user_for_budget_id(self):
+        """
+        Prompts the user to select a budget.
+        """
+        console = Console()
+
+        console.print(Rule(title="[bold]Select a budget from the list below:[/]"))
+
+        for num, (budget_name, budget_id) in enumerate(self.all_budgets.items()):
+            console.print(Markdown(f"{num + 1}. {budget_name}"))
+
+        selected = int(
+            Prompt(
+                "Enter a number: ",
+                console=console,
+                show_choices=True,
+                choices=list(str(i) for i in list(range(1, len(self.all_budgets) + 1))),
+            ).ask()
+        )
+
+        self.selected_budget = list(self.all_budgets.values())[selected - 1]
+
+        console.print(Rule())
+
+    def _get_transactions(self) -> Any:
+        """
+        Gets all transactions associated with the budget.
+        """
+
+        params: dict[str, str] = {"since_date": self.since_date.strftime("%Y-%m-%d")}
+
+        url = self.urls["transactions"].format(self.selected_budget)
+        response = requests.get(url, headers=self.request_headers, params=params)
+        return response.json()["data"]["transactions"]
+
+    def _filter_transactions(self, transactions: Any) -> Any:
+        """
+        Filters transactions by date.
+        """
+
+        search_by = re.compile(r"^.*[[aA]mazon|AMZN].*$")
+
+        filtered_transactions = filter(
+            lambda item: search_by.match(item["payee_name"])
+            and (item["memo"] in ["", None]),
+            transactions,
+        )
+
+        return filtered_transactions
+
+    def _parse_filtered_transactions(self, filtered_transactions: Any) -> None:
+        """
+        Parses the transactions.
+        """
+        for transaction in filtered_transactions:
+            self.transactions_to_match[transaction["id"]] = {
+                "amount": transaction["amount"],
+                "date": datetime.strptime(transaction["date"], "%Y-%m-%d").date(),
+                "payee": transaction["payee_name"],
+                "memo": transaction["memo"],
+            }
+            print(self.transactions_to_match[transaction["id"]])
